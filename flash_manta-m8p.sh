71143aa2b4e7a7ed7951ecd729d2aaac73c215b3
cuiviemen <cuiviemen@gmail.com>
2025-02-26 16:16:53 +0100

    Updated/fixed flash_manta-m8p.sh

--- flash_manta-m8p.sh
+++ flash_manta-m8p.sh
@@ -2,13 +2,16 @@
 
 config="manta-m8p-v11.config"
 canuuid="e62ea4016321"
+deviceid="usb-katapult_stm32g0b1xx_13001C000C504B4633373520-if00"
 
 pushd ~/klipper
 
 make clean
 make menuconfig KCONFIG_CONFIG=$config
 make KCONFIG_CONFIG=$config
-python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u $canuuid
+
+python3 ~/katapult/scripts/flashtool.py -i can0 -r -u $canuuid
+python3 ~/katapult/scripts/flashtool.py -f ~/klipper/out/klipper.bin -d /dev/serial/by-id/$deviceid
 
 popd
 
